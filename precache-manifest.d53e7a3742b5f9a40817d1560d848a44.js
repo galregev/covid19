73d4e0a5ad02a6efbbcf815a92db2f982f2a855b
galregev <galregev55@gmail.com>
2020-04-24 00:50:15 +0300

    Updates

---
+++ precache-manifest.d53e7a3742b5f9a40817d1560d848a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bde9449bfe9eb8afac7bb780db4456",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "3b0b58e30fe61e184bba",
+    "url": "/covid19/static/css/main.a27c088d.chunk.css"
+  },
+  {
+    "revision": "7d4fe2a52f68b0c4837d",
+    "url": "/covid19/static/js/2.f56d337c.chunk.js"
+  },
+  {
+    "revision": "e41c895e71d07deba6491cb940514a48",
+    "url": "/covid19/static/js/2.f56d337c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0b58e30fe61e184bba",
+    "url": "/covid19/static/js/main.7d51cd11.chunk.js"
+  },
+  {
+    "revision": "7df9464acecb6b570615",
+    "url": "/covid19/static/js/runtime-main.4f2a9b22.js"
+  }
+]);
