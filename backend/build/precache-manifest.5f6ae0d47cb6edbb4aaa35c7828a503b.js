45a037ae42102994b3cccba508d539e23e7e69bc
galregev <galregev55@gmail.com>
2020-04-25 17:58:15 +0300

    make it better

---
+++ backend/build/precache-manifest.5f6ae0d47cb6edbb4aaa35c7828a503b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80798b06bbba1008326a1b6d25f202d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee9a145b2e1f3ed9c2c",
+    "url": "/static/css/main.4e72d88d.chunk.css"
+  },
+  {
+    "revision": "992976d73018694ca87b",
+    "url": "/static/js/2.a01c850f.chunk.js"
+  },
+  {
+    "revision": "e41c895e71d07deba6491cb940514a48",
+    "url": "/static/js/2.a01c850f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee9a145b2e1f3ed9c2c",
+    "url": "/static/js/main.cfb95080.chunk.js"
+  },
+  {
+    "revision": "04d90ba97e6e144d96e8",
+    "url": "/static/js/runtime-main.ec654076.js"
+  }
+]);
